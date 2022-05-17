b2f2fb60c5f68d870604d6ef274aeb3f869b39ef
usb <56985400+U5B@users.noreply.github.com>
2022-05-17 15:27:20 -0700

    poi is still not working

--- scripts/monumenta/poi.ts
+++ scripts/monumenta/poi.ts
@@ -1,26 +1,37 @@
-import { isConstructorDeclaration } from 'typescript'
+/* global World, Player, JsMacros, JavaWrapper, event, Chat, Java */
 import poiData from './data/pois.json'
-let pois = {}
-let poiSuggestions = []
+const pois = {}
+const poiSuggestions = []
 
 const map = {
-  'isles': 'isles',
+  isles: 'isles',
   'isles-2': 'isles',
   'isles-3': 'isles',
-  'valley': 'valley',
+  valley: 'valley',
   'valley-2': 'valley',
   'valley-3': 'valley'
 }
 
+const command = Chat.createCommandBuilder('poi')
+
 function makeSearchTerms () {
   for (const [poiName, poi] of Object.entries(poiData)) {
-    let modified = poi
     pois[poi.name] = poi
     pois[poi.name].search = {
       name: poi.name
     }
     poiSuggestions.push(poi.name)
   }
+}
+
+function handleSuggest (ctx) {
+  Chat.log(ctx)
+  return true
+}
+
+function runCommand (ctx) {
+  Chat.log(ctx)
+  return true
 }
 
 makeSearchTerms()
@@ -30,6 +41,9 @@ function debug (input) { // node debug
   // @ts-ignore
   if ((typeof process !== 'undefined') && (process.release.name.search(/node|io.js/) !== -1)) {
     // @ts-ignore
-     console.log(input)
+    console.log(input)
   }
 }
+command.executes(JavaWrapper.methodToJava(runCommand))
+command.suggest(JavaWrapper.methodToJava(handleSuggest))
+command.register()
