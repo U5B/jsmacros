c96dd95f57292902fe001dbe93a58623a832bd8d
usb <56985400+U5B@users.noreply.github.com>
2022-05-17 21:32:16 -0700

    finished refactoring poi

--- scripts/monumenta/poi.ts
+++ scripts/monumenta/poi.ts
@@ -1,6 +1,5 @@
 /* global World, Player, JsMacros, JavaWrapper, event, Chat, Java */
 import poiData from './data/pois.json'
-const pois = {}
 const poiSuggestions = []
 
 const map = {
@@ -16,27 +15,83 @@ const command = Chat.createCommandBuilder('poi')
 
 function makeSearchTerms () {
   for (const [poiName, poi] of Object.entries(poiData)) {
-    pois[poi.name] = poi
-    pois[poi.name].search = {
-      name: poi.name
-    }
     poiSuggestions.push(poi.name)
   }
 }
 
-function handleSuggest (ctx) {
-  Chat.log(ctx)
-  return true
+function searchPoi (input) {
+  const response = []
+  for (const [name, content] of Object.entries(poiData)) { // exact match
+    if (name.includes(input)) return content
+  }
+  /*
+  for (const [name, content] of Object.entries(poiData)) { // fuzzy match
+    if (cleanString(name).includes(cleanString(input))) return content
+  }
+  */
+  for (const [name, content] of Object.entries(poiData)) { // tag match
+    const tags = trimString(name).split(' ')
+    if (tags.includes(trimString(input))) response.push(content)
+  }
+  return response
 }
 
 function runCommand (ctx) {
-  Chat.log(ctx)
+  const poiInput = ctx.getArg('poi')
+  const response = searchPoi(poiInput)
+  if (!response || (Array.isArray(response) && response.length === 0)) {
+    Chat.log(`${poiInput}: No POI found.`)
+    return false
+  } else if (Array.isArray(response)) {
+    for (const rep of response) {
+      Chat.log(`${rep.name} in ${rep.shard}: (${rep.coordinates.x}, ${rep.coordinates.y}, ${rep.coordinates.z})`)
+    }
+  } else {
+    // @ts-ignore
+    Chat.log(`${response.name} in ${response.shard}: (${response.coordinates.x}, ${response.coordinates.y}, ${response.coordinates.z})`)
+  }
   return true
 }
 
-makeSearchTerms()
-debug(poiSuggestions)
+function start () {
+  logInfo('Starting service...')
+  Chat.getLogger('usb').warn('[POI] Starting service...')
+  makeSearchTerms()
+  command.greedyStringArg('poi').suggestMatching(poiSuggestions)
+  command.executes(JavaWrapper.methodToJava(runCommand))
+  command.register()
+  return true
+}
 
+function terminate () {
+  logInfo('Stopping service...')
+  Chat.getLogger('usb').fatal('[POI] Stopping service...')
+  command.unregister()
+  return true
+}
+
+function logInfo (string) {
+  Chat.log(`§7[§aPOI§7] §e${string}`)
+}
+
+function cleanString (str) {
+  return str
+    .replaceAll(/'/g, '')
+    .replaceAll(/\n/g, '')
+    .replaceAll(/ /g, '')
+    .trim()
+    .toLowerCase()
+}
+
+function trimString (str) {
+  return str
+    .trim()
+    .toLowerCase()
+}
+
+start()
+// @ts-ignore
+event.stopListener = JavaWrapper.methodToJava(terminate)
 function debug (input) { // node debug
   // @ts-ignore
   if ((typeof process !== 'undefined') && (process.release.name.search(/node|io.js/) !== -1)) {
@@ -44,6 +99,3 @@ function debug (input) { // node debug
     console.log(input)
   }
 }
-command.executes(JavaWrapper.methodToJava(runCommand))
-command.suggest(JavaWrapper.methodToJava(handleSuggest))
-command.register()
