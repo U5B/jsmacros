2afda15c25142a23c62f9ec82286f03dde028a63
usb <56985400+U5B@users.noreply.github.com>
2022-05-23 19:00:10 +0000

    fix compass

--- scripts/leftClickCompass.ts
+++ scripts/leftClickCompass.ts
@@ -6,25 +6,27 @@ function getCompass () {
 }
 
 function onKeyPress (event) {
-  if (event.key == 'key.attack' && event.action == 0) { // trigger on attack
-    const item = Player.getPlayer().getMainHand().getItemID() // get id of currently held item
-    switch (item) {
-      case 'minecraft:compass': { // if it is a compass, make a special string
-        const coord = getCompass()
-        const builder = Chat.createTextBuilder()
-        builder.append(`§7[§aCOMPASS§7]§r `)
-        const coordinates = `(${coord.x}, ${coord.y}, ${coord.z})`
-        builder.append(coordinates)
-        builder.withColor(0xa)
-        builder.append(' [COPY]')
-        builder.withColor(0xc)
-        builder.withClickEvent('copy_to_clipboard', coordinates)
-        builder.withShowTextHover(Chat.createTextHelperFromString('Click to copy coordinates to clipboard.'))
-        Chat.log(builder.build())
-        break
+  if (World && World.isWorldLoaded()) {
+    if (event.key == 'key.attack' && event.action == 0) { // trigger on attack
+      const item = Player.getPlayer().getMainHand().getItemID() // get id of currently held item
+      switch (item) {
+        case 'minecraft:compass': { // if it is a compass, make a special string
+          const coord = getCompass()
+          const builder = Chat.createTextBuilder()
+          builder.append(`§7[§aCOMPASS§7]§r `)
+          const coordinates = `(${coord.x}, ${coord.y}, ${coord.z})`
+          builder.append(coordinates)
+          builder.withColor(0xa)
+          builder.append(' [COPY]')
+          builder.withColor(0xc)
+          builder.withClickEvent('copy_to_clipboard', coordinates)
+          builder.withShowTextHover(Chat.createTextHelperFromString('Click to copy coordinates to clipboard.'))
+          Chat.log(builder.build())
+          break
+        }
       }
+      return true
     }
-    return true
   }
   return false
 }
